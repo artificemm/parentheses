8240259b9ecbb9fcf08ac91af489aa3970529e55
Roberto Ruiz <artificemm@gmail.com>
2021-04-22 23:09:09 -0500

    linting.

--- parentheses.rb
+++ parentheses.rb
@@ -1,11 +1,13 @@
-#!usr/local/ruby
+# frozen_string_literal: true
 
 def check_parentheses(argument)
   data = argument.chars
   return false if argument.size > 100
+
   stack = 0
   data.each do |current_char|
     return false if current_char == ')' && stack.zero?
+
     stack += 1 if current_char == '('
     stack -= 1 if current_char == ')'
   end
