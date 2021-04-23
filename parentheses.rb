0abe18a6e88b945e10be418a8f66e0005546abe7
Roberto Ruiz <artificemm@gmail.com>
2021-04-22 23:06:10 -0500

    ammend, following js version and return the boolean, previous puts was for cosmetic reasons, now it adheres to requirements

--- parentheses.rb
+++ parentheses.rb
@@ -2,16 +2,12 @@
 
 def check_parentheses(argument)
   data = argument.chars
-  return result(false) if argument.size > 100
+  return false if argument.size > 100
   stack = 0
   data.each do |current_char|
-    return result(false) if current_char == ')' && stack.zero?
+    return false if current_char == ')' && stack.zero?
     stack += 1 if current_char == '('
     stack -= 1 if current_char == ')'
   end
-  result(stack.zero?)
-end
-
-def result(object)
-  puts object
+  stack.zero?
 end
